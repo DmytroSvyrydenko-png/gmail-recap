4475223f9d9dd2bc96ffb241136ca68aad1b5ea3
Dmytro Svyrydenko <20019dima@gmail.com>
2024-01-16 16:36:00 +0200

    add time criteria

--- email_stat.py
+++ email_stat.py
@@ -1,15 +1,19 @@
 import imaplib
+from datetime import datetime
+
 
 gmail_username = input("Enter your Gmail email address: ")
 gmail_password = input("Enter your email password (special for applications): ")
 
 mail = imaplib.IMAP4_SSL("imap.gmail.com")
-
 mail.login(gmail_username, gmail_password)
 
 mail.select("INBOX")
 
-search_criteria = '(ALL)'
+start_date = datetime(2023, 1, 1)
+end_date = datetime(2023, 12, 31)
+
+search_criteria = f'(SINCE {start_date.strftime("%d-%b-%Y")}) (BEFORE {end_date.strftime("%d-%b-%Y")})'
 
 status, messages = mail.search(None, search_criteria)
 
