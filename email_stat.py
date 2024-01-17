1d40c65412c4455f5656804582505e7cc9993981
Dmytro Svyrydenko <20019dima@gmail.com>
2024-01-17 22:15:17 +0200

    added two more criteria

--- email_stat.py
+++ email_stat.py
@@ -14,7 +14,12 @@ def get_count_of_emails(id, folder_code):
     start_date = datetime(2023, 1, 1)
     end_date = datetime(2023, 12, 31)
 
-    search_criteria = f'(SINCE {start_date.strftime("%d-%b-%Y")}) (BEFORE {end_date.strftime("%d-%b-%Y")})'
+    if id == 'inbox' or id == 'sent' or id == 'drafts' or id == 'flagged':
+        search_criteria = f'(SINCE {start_date.strftime("%d-%b-%Y")}) (BEFORE {end_date.strftime("%d-%b-%Y")})'
+    elif id == 'unseen':
+        search_criteria = f'(UNSEEN) (SINCE {start_date.strftime("%d-%b-%Y")}) (BEFORE {end_date.strftime("%d-%b-%Y")})'
+    elif id == 'i_sent_to_me':
+        search_criteria = f'(SINCE {start_date.strftime("%d-%b-%Y")} BEFORE {end_date.strftime("%d-%b-%Y")} FROM {gmail_username} TO {gmail_username})'
 
     status, messages = mail.search(None, search_criteria)
 
@@ -35,6 +40,8 @@ get_count_of_emails('inbox', email_codes[0][1])
 get_count_of_emails('sent', email_codes[5][1])
 get_count_of_emails('drafts', email_codes[8][1])
 get_count_of_emails('flagged', email_codes[6][1])
+get_count_of_emails('unseen', email_codes[0][1])
+get_count_of_emails('i_sent_to_me', email_codes[5][1])
 
 print(result)
 
